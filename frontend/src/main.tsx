a03c39718f8326fe63a155395d21a39c97d33a57
Harshit <hs8700882@gmail.com>
2026-05-01 10:54:31 +0530

    updated tsconfig.json

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import './index.css'  // Standard Vite CSS import
+import App from './App' // No extension needed now
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
